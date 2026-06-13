63742cd2adc98fff58a511d4605de88a4db49acb
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-06-13 17:43:05 +0300

    a bunch of templates

---
+++ portfolio/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import logo from '$lib/assets/logo.png'
+</script>
+<header>
+    <img src={logo} alt="joenikkai logo">
+    <h1>joenikkai</h1>
+</header>
+<style>
+    * {
+        font-family:system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+</style>
